9bfaa49475cddff1344318fb1b4b5af49cbe0f58
David <digital0david@gmail.com>
2019-05-14 23:52:52 -0400

    get user by id

--- users/userRouter.js
+++ users/userRouter.js
@@ -22,13 +22,26 @@ router.get('/', async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({
-            message: 'Error retreiving users'
+            message: 'Error retrieving users'
         });
     }
 });
 
 router.get('/:id', async (req, res) => {
+    try {
+        const user = await Users.getById(req.params.id);
 
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error retrieving the user'
+        })
+    }
 });
 
 router.get('/:id/posts', async (req, res) => {
