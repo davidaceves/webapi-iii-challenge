45b2a7e470c8cc301c834c71cd98bc6a992f801a
David <digital0david@gmail.com>
2019-05-14 21:48:43 -0400

    server/index

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
@@ -6,10 +6,15 @@ server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
+server.use(express.json());
+
+server.use(logger);
+
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(req, res, next);
+  next();
 };
 
 module.exports = server;
